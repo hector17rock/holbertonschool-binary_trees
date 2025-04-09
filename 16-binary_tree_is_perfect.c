8da3ffd82ea0946f76766c37cb4b1c4e3a2075ab
Hector Soto <hector_17rock@hotmail.com>
2025-04-09 11:35:56 -0400

    Add binary tree insert left implementation

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,55 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Height of the tree. If NULL, return 0.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0, right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (left_height > right_height ? left_height : right_height);
+}
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: Pointer to the root node
+ *
+ * Return: Size (number of nodes). If NULL, return 0.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: Pointer to the root node
+ *
+ * Return: 1 if perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height, expected_nodes, actual_nodes;
+
+	if (tree == NULL)
+		return (0);
+
+	height = binary_tree_height(tree);
+	expected_nodes = (1 << (height + 1)) - 1;
+	actual_nodes = binary_tree_size(tree);
+
+	return (actual_nodes == expected_nodes);
+}
+

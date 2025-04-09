b9939812a338d68dd67b152e9d964a5bbb7c21a8
Hector Soto <hector_17rock@hotmail.com>
2025-04-09 11:27:56 -0400

    Add binary tree insert left implementation

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -20,6 +20,26 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
+ * binary_tree_height_balance - Measures the height of a binary tree
+ * for balance calculation
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Height. If tree is NULL, return -1 (for balance factor calculation).
+ */
+int binary_tree_height_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (-1);
+
+	left_height = binary_tree_height_balance(tree->left);
+	right_height = binary_tree_height_balance(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree
  *
@@ -31,8 +51,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	return (
-			(int)binary_tree_height(tree->left) -
-			(int)binary_tree_height(tree->right)
+			binary_tree_height_balance(tree->left) -
+			binary_tree_height_balance(tree->right)
 		   );
 }
 
